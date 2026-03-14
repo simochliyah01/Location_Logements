a4b9264fba13755bba9269be740646bedfdb09cb
simochliyah01 <simochliyah800@gmail.com>
2026-03-14 16:58:04 +0000

    Add Vercel + Supabase config

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,9 @@ pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 
+from decouple import config
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,15 +86,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'location_logements',
-        'USER': 'root',         
-        'PASSWORD': '',         
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
