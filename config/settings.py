289645612ba0b0c68ec163759ffc61778a9fe9a9
simochliyah01 <simochliyah800@gmail.com>
2026-03-14 21:50:04 +0000

    Add Cloudinary + WhiteNoise config

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
